4acc251785186a8d138f0cb21c66e7328c036fdd
Supreme Cat <102873182+supreme-cat@users.noreply.github.com>
2024-10-25 21:38:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["🪐 Playing Nexus Roleplay"];
+const statusTypes = [ 'online' ];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
